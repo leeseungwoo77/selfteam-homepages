6a0f0bbba079d45b9d69d48d4e309862d00e67a6
leeseungwoo77 <sangsangplexhq1@sangsangplex.com>
2026-07-08 11:25:37 +0900

    Add files via upload

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -3,10 +3,10 @@
 // 아래에 그대로 붙여넣으세요. (apiKey, authDomain 등)
 // ==========================================================
 export const firebaseConfig = {
-  apiKey: "AIzaSyBxa9jfr_Qdxyw7l4oLvdEGH4vFa6e0jkA",
-  authDomain: "selfteam-homepage.firebaseapp.com",
-  projectId: "selfteam-homepage",
-  storageBucket: "selfteam-homepage.firebasestorage.app",
-  messagingSenderId: "22289722957",
-  appId: "1:22289722957:web:f5ddcf1407bc78726e7604"
+  apiKey: "AIzaSyDyLC9SYM0vtNtaahuezIYiEC99QzfcDNs",
+  authDomain: "selfteam-homepage-91564.firebaseapp.com",
+  projectId: "selfteam-homepage-91564",
+  storageBucket: "selfteam-homepage-91564.firebasestorage.app",
+  messagingSenderId: "708745145673",
+  appId: "1:708745145673:web:4098303a96170fedfc3e98"
 };
